b5d7595637b5f804b4e79479d4d03175abeba743
Trey Dedman <treydedman@gmail.com>
2024-11-20 20:00:24 +0000

    prep-javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+console.log('Connected!');
+
+const number1 = 1;
+const number2 = 2;
+const product = number1 * number2;
+console.log(product);
+console.log(typeof product);
+
+const charge = 4;
+const payment = 3;
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+const tests = 95;
+const assignments = 100;
+const final = 90;
+const grade = (tests + assignments + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Trey';
+const lastName = 'Dedman';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+const pH = 5;
+const isAcidic = pH < 7;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+const numberOfSoldiers = 500;
+const isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+let nickname = fullName + ' is the GOAT';
+console.log(nickname);
+console.log(typeof nickname);
